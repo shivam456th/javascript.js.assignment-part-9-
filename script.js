103dd18f5273398c45719d7de8094f1291dc8b3d
shivam456th <shivamthapa792@gmail.com>
2024-03-10 13:10:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let inp = document.createElement("input");
+let btn = document.createElement("button");
+let h1 = document.createElement("h1");
+let body = document.querySelector("body");
+let p = document.querySelector("p")
+p.style.fontWeight = 'bold';
+
+body.appendChild(p);
+body.appendChild(h1);
+body.appendChild(inp);
+body.appendChild(btn);
+
+
+h1.innerText="shivam";
+btn.innerText="click me!";
+
+h1.style.textDecoration = "underline overline";
+h1.style.color="purple";
+btn.style.backgroundColor="blue";
+btn.style.color="white";
+
+
+
+
+
+
+
+
+
